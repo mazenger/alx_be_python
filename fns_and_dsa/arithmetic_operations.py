b5dc65e904fba5f6a1b0ec61b80b1928d8e466b6
Mazenaly <50014979+mazenger@users.noreply.github.com>
2025-06-09 00:29:13 +0300

    commit2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,8 @@ def perform_operation(num1: float, num2: float, operation: str):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num1 == 0:
-            print("cant divide zero")
-        else:
-            return num1 / num2
-
-print(perform_operation(0, 2, "divide"))
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
